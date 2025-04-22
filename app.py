4999d47d6db31c3fe832285d15a608ac0ca72a4d
aleesssa <aleessab@gmail.com>
2025-04-22 11:20:11 +0800

    Created file structure and base for app

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)

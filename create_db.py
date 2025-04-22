773a7c78998e15bf0e08055dfaa565520dc92ff9
aleesssa <aleessab@gmail.com>
2025-04-23 04:27:33 +0800

    Added requirements.txt file and create_db.py

---
+++ create_db.py
@@ -0,0 +1,14 @@
+from app import db
+from models.user import User
+from models.request import Request
+from models.chat_message import ChatMessage
+from models.rating import Rating
+from models.receipt import Receipt
+from models.store import Store
+
+# Drop existing tables (optional during testing)
+# db.drop_all()
+
+# Create tables
+db.create_all()
+print("✅ Database created!")

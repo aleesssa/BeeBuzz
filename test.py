715f37c373025f545903df65db58f7556956f1b5
aleesssa <aleessab@gmail.com>
2025-04-24 02:15:44 +0800

    Added another user to database

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from models.user import User
 from app import app, db
 
-user = User(
+user1 = User(
     username="aleessa",
     email="aleessa@mmu.edu.my",
     phone_number="0123456789",
@@ -10,7 +10,16 @@ user = User(
     profile_image="default.png"
 )
 
+user2 = User(
+    username = 'Jaemin',
+    email = 'jaemin@gmail.com',
+    phone_number = '0104523053',
+    password = '1111111',
+    isRider=False,
+    profile_image='image.png'
+)
+
 with app.app_context():
-    db.session.add(user)
+    db.session.add(user2)
     db.session.commit()
-    print("✅ User added:", user.username)
+    print("✅ User added:", user2.username)

4a15948bc9cdc4c52f20aea068fca5be57edc8a0
Nur Damia' Batrisyia <damiabatrisyia108@gmail.com>
2025-04-24 12:55:12 +0800

    added request blueprint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from extensions import db
 
 app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
@@ -12,8 +12,11 @@ db.init_app(app)
 
 # Register blueprints
 from routes.chat_routes import chat_bp
+from routes.request_routes import request_bp
 
 app.register_blueprint(chat_bp, url_prefix='/chat')
+app.register_blueprint(request_bp, url_prefix='/request')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+

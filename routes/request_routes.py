f4c8ade9be2752eb12bf58faf66a2be123f1e217
Nur Damia' Batrisyia <damiabatrisyia108@gmail.com>
2025-05-07 13:06:45 +0800

    trying to fix error

--- routes/request_routes.py
+++ routes/request_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, render_template, url_for, redirect
 from extensions import db
 
-request_bp = Blueprint('request', __name__) # Equivalent to app = Flask(__name__)
+request_bp = Blueprint('request_bp', __name__) # Equivalent to app = Flask(__name__)
 
 @request_bp.route('/')
 def request():
@@ -9,14 +9,16 @@ def request():
 
 @request_bp.route('/request', methods=['POST','GET'])
 def handle_request():
+    if request.method == 'GET':
+        return render_template('request.html')
     if request.method == 'POST':
-      form_data = {
-        "need": request.form.get("need"),
-        "payment": request.form.get("payment"),
-        "time":  request.form.get("time"),
-        "pickup": request.form.get("pickup"),
-        "dropoff": request.form.get("dropoff"),
-        "notes": request.form.get("notes"),
-      }
-      return render_template("summaryreq.html", data=form_data)
+        need = request.form.get("need"),
+        payment = request.form.get("payment"),
+        time = request.form.get("time"),
+        pickup = request.form.get("pickup"),
+        dropoff = request.form.get("dropoff"),
+        notes = request.form.get("notes"),
+        return render_template ("summaryreq.html")
+        
+
     

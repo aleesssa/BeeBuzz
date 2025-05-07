0f714c71a72d6ece6563acddf73736664b8869b4
Nur Damia' Batrisyia <damiabatrisyia108@gmail.com>
2025-05-08 07:28:50 +0800

    Added notes in database

--- routes/request_routes.py
+++ routes/request_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template, url_for, redirect
 from extensions import db
+from models.request import Request
 
 request_bp = Blueprint('request_bp', __name__) # Equivalent to app = Flask(__name__)
 
@@ -7,18 +8,20 @@ request_bp = Blueprint('request_bp', __name__) # Equivalent to app = Flask(__nam
 def request():
    return render_template ("request.html")
 
-@request_bp.route('/request', methods=['POST','GET'])
+@request_bp.route('/request', methods=['GET','POST'])
 def handle_request():
-    if request.method == 'GET':
-        return render_template('request.html')
     if request.method == 'POST':
-        need = request.form.get("need"),
-        payment = request.form.get("payment"),
-        time = request.form.get("time"),
-        pickup = request.form.get("pickup"),
-        dropoff = request.form.get("dropoff"),
-        notes = request.form.get("notes"),
-        return render_template ("summaryreq.html")
+        form_data ={
+            "need": request.form.get("need"),
+            "payment": request.form.get("payment"),
+            "time": request.form.get("time"),
+            "pickup": request.form.get("pickup"),
+            "dropoff": request.form.get("dropoff"),
+            "notes": request.form.get('notes'),
+        }
+        db.session.commit()
+        return render_template ("summaryreq.html", data=form_data)
+    return render_template("request.html")
         
 
     

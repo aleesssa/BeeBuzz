aaf01d0226b8d473dbd314092a8d5e37fe9db8a4
aleesssa <aleessab@gmail.com>
2025-05-08 12:34:23 +0800

    Updated User table

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 from extensions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone_number = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    isRider = db.Column(db.Boolean, nullable=False, default=False)
-    profile_image = db.Column(db.String(255))
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+    phone = db.Column(db.String(20), nullable=True)
+    profile_pic = db.Column(db.String(120), nullable=False, default='default.jpg')
+    role = db.Column(db.String(20), default='shopper')
+    reset_code = db.Column(db.String(6), nullable=True)
+    reset_code_expiry = db.Column(db.DateTime, nullable=True)

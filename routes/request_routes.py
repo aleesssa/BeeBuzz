f3393a096065070fd682a001b84348c7b339d976
Nur Damia' Batrisyia <damiabatrisyia108@gmail.com>
2025-05-12 00:03:11 +0800

    added status cancel

--- routes/request_routes.py
+++ routes/request_routes.py
@@ -12,7 +12,21 @@ def request_job():
 @request_bp.route('/req', methods=['GET','POST'])
 def handle_request():
     if request.method == 'POST':
-        status=request.form.get("request")
+        if "cancel" in request.form:
+         cancelled_request = Request(
+            item_name=request.form.get("need"),
+            price_offer=request.form.get("payment"),
+            time=request.form.get("time"),
+            pickup_location=request.form.get("pickup"),
+            dropoff_location=request.form.get("dropoff"),
+            notes=request.form.get("notes"),
+            created_at=datetime.utcnow(),
+            client_id=session.get('user_id'),
+            status= 'cancelled'
+              )
+        
+        db.session.add(cancelled_request)
+        db.session.commit()
 
         new_request = Request(
             item_name=request.form.get("need"),
@@ -22,7 +36,8 @@ def handle_request():
             dropoff_location=request.form.get("dropoff"),
             notes=request.form.get("notes"),
             created_at=datetime.utcnow(),
-            client_id=session.get('user_id')
+            client_id=session.get('user_id'),
+            status='requested'
               )
         
         db.session.add(new_request)

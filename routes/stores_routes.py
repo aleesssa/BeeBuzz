f1bf7ac789864622994cfe83c1f8ee8de862e34f
aleesssa <aleessab@gmail.com>
2025-05-29 23:11:36 +0800

    Added check store status function

--- routes/stores_routes.py
+++ routes/stores_routes.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, request, render_template, jsonify, session, current_app
 from extensions import db
-from models.chat_message import ChatMessage
-from models.user import User
-from models.request import Request
 from models.store import Store
+from datetime import datetime
 
 
 stores_bp = Blueprint('stores', __name__) # Equivalent to app = Flask(__name__)
@@ -13,11 +11,30 @@ stores_bp = Blueprint('stores', __name__) # Equivalent to app = Flask(__name__)
 def stores():
     return render_template('stores.html')
 
+
 @stores_bp.route('/add', methods = ['GET', 'POST'])
 def add_store():
     if request.method == "GET":
         return render_template('store_add.html')
 
     if request.method == "POST":
+        name = request.form.get('name')
+        time_open = request.form.get('time_open')
+        time_close = request.form.get('time_close')
+        
+        
+        store = Store(
+            name = name,
+            time_open = time_open,
+            time_close = time_close
+        )
+        
+        db.session.add(store)
+        db.session.commit()
+        
         return 0
         
+# Check if store is open or close
+def is_open(time_open, time_close):
+    now = datetime.now().time()
+    return time_open <= now < time_close

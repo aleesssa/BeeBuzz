99d48549bdef4d1c3d488e0cd02b10f017af2008
aleesssa <aleessab@gmail.com>
2025-05-29 22:07:56 +0800

    Updated tables request and store

--- models/store.py
+++ models/store.py
@@ -4,5 +4,7 @@ from datetime import datetime
 class Store(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    time_open = db.Column(db.Time, nullable=False)
+    time_close = db.Column(db.Time, nullable=False)
     status = db.Column(db.String(20), nullable=False)  # e.g., Open, Closed
-    last_updated = db.Column(db.DateTime, default=datetime.utcnow)
+    last_updated = db.Column(db.DateTime, default=datetime.now().time())

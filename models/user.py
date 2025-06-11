f5b4c004e440b49255a941e3b5548e014f037f44
Nur Syaza Aqilah Binti Aziz <NUR.SYAZA.AQILAH@student.mmu.edu.my>
2025-06-11 15:44:46 +0800

    userpy update

--- models/user.py
+++ models/user.py
@@ -5,11 +5,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-<<<<<<< HEAD
-    nickname = db.Column(db.String(150), unique=True, nullable=True)
-=======
     nickname = db.Column(db.String(150), nullable=True)
->>>>>>> auth
     password_hash = db.Column(db.String(256), nullable=False)
     phone = db.Column(db.String(20), nullable=True)
     profile_pic = db.Column(db.String(120), nullable=False, default='default.jpg')

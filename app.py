8517e53cba22a636b6199821421d426811e68556
aleesssa <aleessab@gmail.com>
2025-06-18 23:55:34 +0800

    Moved eventlet to top;

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from dotenv import load_dotenv
 from flask import Flask, render_template
@@ -13,8 +16,6 @@ from routes.stores_routes import stores_bp
 from routes.auth_routes import auth_bp
 from routes.request_routes import request_bp
 
-import eventlet
-eventlet.monkey_patch()
 
 load_dotenv()
 
